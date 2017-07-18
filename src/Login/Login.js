3c8720bcf43a15c1d93cedd7195d12fa36b2151e
Chanathip <neetchanathip@gmail.com>
2017-07-18 17:13:59 +0700

    change login page layout

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -2,7 +2,7 @@
  * Created by neetc on 7/15/2017.
  */
 import React, {Component} from 'react';
-import {Button, Form, Icon} from 'semantic-ui-react';
+import {Button, Form} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 import './Login.css';
 
@@ -11,34 +11,26 @@ class LoginFrom extends Component {
         return (
             <div className="Login">
                 <Form className="Login-Form" size="big">
-                    <h1 className="Form-title">Login to App</h1>
+
+                    <h1 className="Form-title">
+                        <Link to="..">
+                            <Button circular icon='arrow left' color='black'/>
+                        </Link>Login to App
+                    </h1>
                     <Form.Field>
                         <input placeholder='email'/>
                     </Form.Field>
                     <Form.Field>
                         <input placeholder='password'/>
                     </Form.Field>
-                    <div className="Action-bar">
-                        <Link to="..">
-                            <div className="Back-button">
-                                <Button animated size='big' type='button'>
-                                    <Button.Content visible>back</Button.Content>
-                                    <Button.Content hidden>
-                                        <Icon name='angle double left'/>
-                                    </Button.Content>
-                                </Button>
-                            </div>
-                        </Link>
-                        <Button animated='fade' size='big' type='submit' positive>
+                    <div className="Action-group">
+                        <Button animated='fade' size='big' type='submit' positive className="Login-button">
                             <Button.Content visible>Login</Button.Content>
                             <Button.Content hidden>Hi!</Button.Content>
                         </Button>
                         <Link to="../Register">
                             <div className="Register-button">
-                                <Button animated='fade' size='big' type='button' basic>
-                                    <Button.Content visible>Register</Button.Content>
-                                    <Button.Content hidden>welcome!</Button.Content>
-                                </Button>
+                                Don't have account? Register!
                             </div>
                         </Link>
                     </div>
