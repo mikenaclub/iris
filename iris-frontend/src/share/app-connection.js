2f0076b88afe8505c0154f3514adedf8f998842e
chanathip <neetchanathip@gmail.com>
2017-08-08 16:10:27 -0700

    change connection string

--- iris-frontend/src/share/app-connection.js
+++ iris-frontend/src/share/app-connection.js
@@ -1,4 +1,4 @@
-const _registerConnetionString = "http://localhost:8091/registerApi/v1/user";
-const _loginConnectionString = "http://localhost:8091/registerApi/v1/login";
+const _registerConnetionString = "http://127.0.0.1:8091/registerApi/v1/user";
+const _loginConnectionString = "http://127.0.0.1:8091/registerApi/v1/login";
 exports.registerConnetionString = _registerConnetionString;
 exports.loginConnectionString = _loginConnectionString;
