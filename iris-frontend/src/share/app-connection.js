7e3853ca74d8036533d57ceb88ac8e066f83d1ac
chanathip <neetchanathip@gmail.com>
2017-08-09 13:01:24 -0700

    fix typo

--- iris-frontend/src/share/app-connection.js
+++ iris-frontend/src/share/app-connection.js
@@ -1,4 +1,4 @@
-const _registerConnetionString = "http://localhost:8091/registerApi/v1/user";
+const _registerConnectionString = "http://localhost:8091/registerApi/v1/user";
 const _loginConnectionString = "http://localhost:8091/registerApi/v1/login";
-exports.registerConnetionString = _registerConnetionString;
+exports.registerConnetionString = _registerConnectionString;
 exports.loginConnectionString = _loginConnectionString;
