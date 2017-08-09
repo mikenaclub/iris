0331fbc427cdc26c491af5f46526320cb885405f
chanathip <neetchanathip@gmail.com>
2017-08-09 12:29:43 -0700

    add for setting reverse proxy for nginx

--- iris-userauth/src/main/java/com/example/demo/SpringBootApiDatabaseApplication.java
+++ iris-userauth/src/main/java/com/example/demo/SpringBootApiDatabaseApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringBootApiDatabaseApplication {
@@ -10,3 +12,11 @@ public class SpringBootApiDatabaseApplication {
 		SpringApplication.run(SpringBootApiDatabaseApplication.class, args);
 	}
 }
+@RestController
+class FirstPage{
+	@RequestMapping("/")
+	public String firstPage(){
+		return "Hello world!";
+	}
+}
+
