879bdfdbd0ab63f8dda12aa3f14df3998396125b
chanathip <neetchanathip@gmail.com>
2017-08-11 13:11:13 -0700

    fix connection string

--- iris-frontend/src/share/app-connection.js
+++ iris-frontend/src/share/app-connection.js
@@ -1,4 +1,4 @@
 
 exports.registerConnetionString = "http://localhost:8091/v1/user";
 exports.loginConnectionString = "http://localhost:8091/v1/login";
-exports.chatConnectionString = "http://localhost:8093/v1";
+exports.chatConnectionString = "http://localhost:8093/";
